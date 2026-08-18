0ce484cb16e66fcb9f36a549ab382abe7f1e5fcb
Anmol Kawade <anmolkawade7@gmail.com>
2026-08-18 21:28:28 +0530

    Fix Render PostgreSQL connection

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
-// db.js
 const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // required for Render Postgres
-  },
+    rejectUnauthorized: false
+  }
+});
+
+pool.on('connect', () => {
+  console.log('Connected to PostgreSQL');
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected PostgreSQL pool error:', err);
 });
 
 module.exports = pool;
