cd015e64221b6e843802f012264f223f34eed03b
Anmol Kawade <anmolkawade7@gmail.com>
2026-08-21 13:47:31 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,18 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.on('connect', () => {
-  console.log('📦 Connected to PostgreSQL');
+  console.log('Connected to PostgreSQL');
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected PostgreSQL pool error:', err);
 });
 
 module.exports = pool;
